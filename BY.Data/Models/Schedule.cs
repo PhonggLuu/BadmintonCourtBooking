74592943f72e3f97155decf511d1815e0300fc9a
huynhduc <lengochuynhduc7@gmail.com>
2024-06-14 09:37:14 +0700

    update schedule

--- BY.Data/Models/Schedule.cs
+++ BY.Data/Models/Schedule.cs
@@ -2,7 +2,6 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace BY.Data.Models;
 
@@ -10,23 +9,24 @@ public partial class Schedule
 {
     public int ScheduleId { get; set; }
 
-    [Required(ErrorMessage = "The Court field is required")]
     public int? CourtId { get; set; }
 
-    [Required]
     public TimeOnly? From { get; set; }
 
-    [Required]
     public TimeOnly? To { get; set; }
 
-    [Required]
     public decimal? Price { get; set; }
 
-    [Required]
     public DateTime? Date { get; set; }
 
-    [Required]
-    public bool? IsBooked { get; set; } = false;
+    public string Notes { get; set; }
+
+    public string StaffCheck { get; set; }
+
+    public string Event { get; set; }
+
+    public bool? IsBooked { get; set; }
+
     public virtual ICollection<BookingDetail> BookingDetails { get; set; } = new List<BookingDetail>();
 
     public virtual Court Court { get; set; }
