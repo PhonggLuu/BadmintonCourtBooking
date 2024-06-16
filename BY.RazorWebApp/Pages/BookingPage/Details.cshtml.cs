ba5459415299f71b87f1e5056e7a0ebbad2e4487
huynhduc <lengochuynhduc7@gmail.com>
2024-06-16 19:22:58 +0700

    Conflict hoangnq

--- BY.RazorWebApp/Pages/BookingPage/Details.cshtml.cs
+++ BY.RazorWebApp/Pages/BookingPage/Details.cshtml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using BY.Data.Models;
-using BY.Busniness;
 using BY.Business;
 
 namespace BY.RazorWebApp.Pages.BookingPage
